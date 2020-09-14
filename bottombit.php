6d0f6c351be204390e87549bd1022c0dcfba6694
shcherbynas <60583431+shcherbynas@users.noreply.github.com>
2020-09-14 18:29:39 +1200

    1 (index)

---
+++ bottombit.php
@@ -0,0 +1,7 @@
+        <div class="box footer">
+            CC Svitlana Shcherbyna 2020
+        </div>    <!-- / footer -->
+    
+    </div>  <!-- / wrapper  -->
+    
+</body> 
---
+++ topbit.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Book reviews database">
+    <meta name="keywords" content="books, reading, fiction, non-fiction, author, genre, review, rating">
+    <meta name="author" content="Svitlana Shcherbyna">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Book reviews</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/book_style.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+        <!-- logo image linking to home page goes here -->
+        <a href="index.php">
+            <div class="box logo"  title="Logo - Click here to go to the Home Page">
+            <img class="img-circle" src="images/books_image.jpg" width="150" height="150" alt="generic logo" />
+            
+            </div>    <!-- / logo -->
+        </a>
+            
+            <h1>Orchid reading</h1>
+        </div>    <!-- / banner -->
+	
+	<div class="box side">
+		<h2>Search     |<a class="side" href="showall.php">Show all</a></h2>
+        <i>Type part of the title / author name if desired</i>
+        <hr/>
+        Title Search <br/>
+        Author Search <br/>
+        Genre Search <br/>
+        Rating Search <br/>
+	</div> <!-- / side bar  -->  
+        
